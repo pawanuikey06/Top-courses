cd66979d15fb6f6154e2fd27a3d1961d9570be32
pawanuikey06 <pawanuikey690@gmail.com>
2024-06-07 16:26:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths as necessary
+    './public/index.html', // If you have HTML files in public directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
